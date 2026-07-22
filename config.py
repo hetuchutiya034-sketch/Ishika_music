9e3c55fc04396e93b2fdd177b77b7642b98e5e43
hetuchutiya034-sketch <hetuchutiya034@gmail.com>
2026-07-22 22:54:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 12345678
+API_HASH = "tera_api_hash"
+BOT_TOKEN = "tera_bot_token"
+OWNER_ID = 123456789
+LOG_GROUP = -1001234567890
+SUPPORT_LINK = "https://t.me/tera_support"
+CHANNEL_LINK = "https://t.me/tera_channel"
