21562ed6b142732f32db765a4db8ccec5777ab0d
hetuchutiya034-sketch <hetuchutiya034@gmail.com>
2026-07-22 23:01:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ API_HASH = "tera_api_hash"
 BOT_TOKEN = "tera_bot_token"
 OWNER_ID = 123456789
 LOG_GROUP = -1001234567890
-SUPPORT_LINK = "https://t.me/tera_support"
-CHANNEL_LINK = "https://t.me/tera_channel"
+SUPPORT_LINK = "https://t.me/ishika_chat"
+CHANNEL_LINK = "https://t.me/ye_duniya_ek_sapna_he"
